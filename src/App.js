3704547575d0b87e4ac102bc42606a4403a04496
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-30 21:09:01 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -13,7 +13,40 @@ function App() {
 }
 
 function Home() {
-  return <h1>HACKATHON 2</h1>;
+  const [file, setFile] = useState();
+
+  const uploadFile = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const reqCred = {
+      method: "POST",
+      // headers: { "Content-Type": "multipart/form-data" },
+      body: formData,
+    };
+
+    const result = await fetch("/posts", reqCred)
+      .then((res) => res.json())
+      .then((data) => console.log(data.id))
+      .catch((err) => console.log(err));
+    console.log(result);
+  };
+
+  return (
+    <div>
+      <h1>HACKATHON 2</h1>
+      <form onSubmit={uploadFile}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(fle) => setFile(fle.target.files[0])}
+        ></input>
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
