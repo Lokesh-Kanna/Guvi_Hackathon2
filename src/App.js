97a8a9bb9d3c95be94bfa3a06bf214b5fea7b45a
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-12-01 10:25:10 +0530

    minor addition

--- src/App.js
+++ src/App.js
@@ -69,6 +69,9 @@ function SecondPage() {
         <button type="submit">Upload</button>
       </form>
       <div>
+        <p>Endpoints: /drive | /users/signup</p>
+      </div>
+      <div>
         {fileDisp.map((file) => {
           return <FileDisplay name={file.fileName} path={file.path} />;
         })}
